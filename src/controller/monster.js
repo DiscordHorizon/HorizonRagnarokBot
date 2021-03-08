18a086637aa489597a04f7c93f731b90d7a9fb3d
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-08 20:33:20 -0300

    fix monsterInfo

--- src/controller/monster.js
+++ src/controller/monster.js
@@ -4,23 +4,38 @@ const { monsterInfo } = require("../include/monsterInfo");
 
 module.exports = {
     async monsterSearch(message, search) {
+        let monsterFound = false;
         const sendMessage = await message.channel.send(
             new MessageEmbed().setTitle("Processando...")
         );
         await monsterModel.find({}, (err, monsters) => {
-            monsters.forEach((monster) => {
+            monsters.every((monster) => {
                 const monsterNameBr = monster.name.ptBr.toLowerCase();
                 const monsterNameEn = monster.name.en.toLowerCase();
                 if (monster.id == search) {
-                    return monsterInfo(sendMessage, monster);
+                    monsterInfo = !monsterFound;
+                    monsterInfo(sendMessage, monster);
+                    return false;
                 }
                 if (monsterNameBr.includes(search)) {
-                    return monsterInfo(sendMessage, monster);
+                    monsterInfo = !monsterFound;
+                    monsterInfo(sendMessage, monster);
+                    return false;
                 }
                 if (monsterNameEn.includes(search)) {
-                    return monsterInfo(sendMessage, monster);
+                    monsterInfo = !monsterFound;
+                    monsterInfo(sendMessage, monster);
+                    return false;
                 }
             });
         });
+        if (!monsterFound) {
+            sendMessage.edit(
+                new MessageEmbed()
+                    .setTitle("Nenhum monstro encontrado.")
+                    .setTimestamp(Date.now())
+                    .setFooter("RagnarokBot by Bravan")
+            );
+        }
     },
 };
