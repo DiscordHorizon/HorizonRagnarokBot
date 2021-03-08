4b89b616d297f60165c75139fc19424a26911c71
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-08 16:17:48 -0300

    create commands

--- src/bot.js
+++ src/bot.js
@@ -1,7 +1,22 @@
 const Discord = require('discord.js');
+const fs = require("fs");
 const { config } = require('./config');
 
 const bot = new Discord.Client();
+bot.commands = new Discord.Collection();
+
+//* ler comandos da pasta "commands"
+fs.readdir("./commands/", (err, files) => {
+    if (err) {
+        console.log(err);
+    }
+    let commandjs = files.filter((f) => f.split(".").pop() == "js");
+    commandjs.forEach((f, i) => {
+        let props = require(`./commands/${f}`);
+        console.log(`[Command] Comando ${f} carregado com sucesso.`);
+        bot.commands.set(props.info.name, props);
+    });
+});
 
 bot.on('ready', async () => {
     await bot.user.setPresence({
@@ -16,6 +31,22 @@ bot.on('ready', async () => {
 
 bot.on('message', async (message) => {
     if (message.author.bot) return;
+
+    const arg = message.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = arg.shift().toLowerCase();
+    let args = arg.shift();
+
+    //* montar string de pesquisa
+    while (arg.length > 0) {
+        args = args + " " + arg[0];
+        arg.shift();
+    }
+    
+    //* executar comando
+    const commandcmd = bot.commands.get(command);
+    if (commandcmd) {
+        commandcmd.run(bot, message, args);
+    }
 })
 
 bot.login(config.tokens.discord);
