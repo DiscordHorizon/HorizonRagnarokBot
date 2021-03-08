2e70e2271a2688df95469f35e1914695aabfa6f1
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-08 16:40:46 -0300

    fix get Monster array

--- src/controller/monster.js
+++ src/controller/monster.js
@@ -1,14 +1,15 @@
 const monsterModel = require("../models/monster");
 
-async function getMonsters() {
-    var monsters = [];
-    await monsterModel.find({} , (err, monster) => {
-        monsters.push(monster);
-    });
-}
-getMonsters();
-
 module.exports = {
+    async getMonsters(message, search, command) {
+        let monsters = [];
+        await monsterModel.find({} , (err, monster) => {
+            monsters.push(monster);
+        });
+        if (command === 'info') {
+            this.getMonsterInfo(message, search);
+        }
+    },
     async getMonsterInfo(message, search) {
         console.log(monsters.find(search));
     },
