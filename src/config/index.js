54ddf7ef95ebaa10b281494bfb3a351dc05d76bc
Matheus Ishiyama <semrumo3@hotmail.com>
2021-03-08 22:25:34 -0300

    add rates

--- src/config/index.js
+++ src/config/index.js
@@ -10,5 +10,11 @@ exports.config = {
         discord: config ? config.discord : process.env.DISCORD,
         mongoUri : config ? config.mongoUri : process.env.MONGO_URI,
     },
-    prefix: config ? config.prefix : process.env.PREFIX
+    prefix: config ? config.prefix : process.env.PREFIX,
+    rates: {
+        base: config ? config.rates.base : process.env.BASE,
+        job: config ? config.rates.job : process.env.JOB,
+        drop: config ? config.rates.base : process.env.DROP,
+        bossDrop: config ? config.rates.base : process.env.BOSS_DROP,
+    }
 }
